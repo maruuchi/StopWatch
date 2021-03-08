a92edc8da8843bd26c4da46d2723de57c722fac4
soma-maruuchi <mwa60346@gmail.com>
2021-03-08 23:56:51 +0900

    jsの変更

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict';
+/* エラーチェック*/
+
+{
+  const timer = document.getElemntById('timer');
+  const start = document.getElemntById('start');
+  const stop = document.getElemntById('stop');
+  const reset = document.getElemntById('reset');
+}
