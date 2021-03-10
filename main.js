d884cc30146b23d15cee1b3d4de50591ada7126c
soma-maruuchi <mwa60346@gmail.com>
2021-03-10 14:56:08 +0900

    各コードの変更

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict';
-/* エラーチェック*/
 
 {
-  const timer = document.getElemntById('timer');
-  const start = document.getElemntById('start');
-  const stop = document.getElemntById('stop');
-  const reset = document.getElemntById('reset');
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
 
   let startTime;
 
@@ -13,9 +12,9 @@
     const d = new Date(Date.now() - startTime);
     const m = String(d.getMinutes()).padStart(2, '0');
     const s = String(d.getSeconds()).padStart(2, '0');
-    const ms = String(d.getMilliseconds()) .padStart(3, '0');
-    timer.textContent ='${m}:${s}.${ms}';
-    
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
     setTimeout(() => {
       countUp();
     }, 10);
