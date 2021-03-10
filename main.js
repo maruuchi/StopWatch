4756247104bb4b4ad9b7bccc8d8c0cf8ae0eff0a
soma-maruuchi <mwa60346@gmail.com>
2021-03-10 14:02:15 +0900

    「時」の追加

--- main.js
+++ main.js
@@ -11,7 +11,12 @@
 
   function countUp() {
     console.log(Date.now() - startTime);
+    const  d = new Date(Date.now() - startTime);
+    setTimeout(() => {
+      countUp();
+    }, 10);
   }
+
   start.addEventListener('click', () => {
     startTime = Date.now();
     countUp();
