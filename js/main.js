dcce388c9491be44af019bed326433f8a24e0c54
soma-maruuchi <mwa60346@gmail.com>
2021-03-11 20:38:08 +0900

    ストップ後にSTARTボタンとRESETボタンのみが表示されるように調整

--- js/main.js
+++ js/main.js
@@ -29,23 +29,33 @@
   }
 
   function setButtonStateRunning() {
-
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
   }
 
   function setButtonStateStopped() {
-
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
   }
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    
     timer.textContent = '00:00:000';
     elapsedTime = 0;
   });
